7e84e0ce174583c134192eb68f545d2d650e064b
carlos <eduardo_10_57@hotmail.com>
2016-08-06 09:55:29 -0500

    agregando para galar items  06/08/2016

--- Business/Boleta.cs
+++ Business/Boleta.cs
@@ -83,7 +83,7 @@ namespace ialbusacpr.Business
             //    + "    order by d.documentoid asc ";
 
 
-            String sSQL = "       select   d.serie|| '-' ||   d.numero as NoTicket,D.DOCUMENTOID , d.fechareal  as FECHA,  "
+            String sSQL = "  select   d.serie|| '-' ||   d.numero as NoTicket,D.DOCUMENTOID , d.fechareal  as FECHA,  "
 
    + "  (select fl.descripcion from facturacion_log  fl  "
 + " where trunc(fl.fecha)  >= '" + f1.ToShortDateString() + "'  AND   trunc(fl.fecha) <= '" + f1.ToShortDateString() + "'  and fl.pkdocumentoid = d.documentoid )as DESCRIPCION,"
@@ -256,7 +256,7 @@ namespace ialbusacpr.Business
                   doc.Emisor.Distrito = "20";
                   doc.Emisor.Direccion = "Jr junin";
                   doc.Emisor.Departamento = "20"; 
-
+              //  doc.Items.Add()
 
 
 
@@ -270,7 +270,147 @@ namespace ialbusacpr.Business
               }
         }
 
+        ///  
+        public Boolean CargaFactura(DocumentoElectronico doc)
+        {
+            OracleConnection Cn = new OracleConnection();
 
+            // var doc = (DocumentoElectronico)_documento.Clone();
+            if (Cn.State == ConnectionState.Closed)
+            {
+                Cn.ConnectionString = "Data Source=maximus;Persist Security Info=True;User ID=db_albusa;Password=db_albusa;Unicode=True";
+                Cn.Open();
+            }
+            String sSQL = " SELECT d.documentoid,  (to_char(d.fecha, 'yyyy') || '-' || to_char(d.fecha, 'mm') || '-' || to_char(d.fecha, 'dd')) as IssueDate, "//- 1 Fecha de emision
+ + " 'INVERSIONES ALBUSA SAC' as RegistrationName, "// 2 Apellidos nombres  denominacion o razon social
+ + "'20484245249' as CustomerAssignedAccountID, "//5 RUC
+ + "'6' as AdditionalAccountID,"//5 
+ + "'03' as InvoiceTypeCode ,"//6 Tipo de documento(Boleta)
+ + "'B' || c.seriedocumentos || '-' || d.numero as ID,"// 7 Numeracion , conformada por serie y numero correlativo
+ + "cl.dni as CustomerAssignedAccountID,"// 8 Tipo y (numero) de documento de identidad del adquiriente o usuario
+ + "'1' as AdditionalAccountID,"//  8 (Tipo) y numero de documento de identidad del adquiriente o usuario
+ + "trim(cl.apellidocliente) || ' ' || trim(cl.nombrecliente) as RegistrationName," //9 Apellidos y nombres de adquiriente o usuario
+                                                                                    //10 Direccion en el pais adquiriente o lugar de destino 
+  + "'1' as unitCode,"//11  Unidad de medidda por Item
+  + "'1' as InvoicedQuantity,"//12 cantidad de unidades  por item 
+  + "'PAGO DE CUOTA DE CREDITO' || '  ' || cr.codigocredito as Description , "//13  DEscrIPCION DETALLA DEL SERVICIO PRESTADO bien vendido 
+  + "'0.00', "//14 precio de venta unitario por item y codigo
+ + " d.montototal as ID ,"//15 total valo de ventas gravadas 
+  + "p.montocapital as PayableAmount,"//16  total valor venta operaciones inafectas
+                                      //17
+  + "p.montointeres," //18
+                      //19
+                      //20
+                      //21
+                      //22
+      + "       p.montototal as currencyID,"//23
+  + "'PEN' as DocumentCurrencyCode,"//24
+                                    //25
+                                    //26
+      + "       p.montoimpuesto as ,"//27
+                                     //28
+                                     //29
+                                     //30
+          + "   p.montointeres,"//31
+   + "p.montointeres"//32
+                     //33
+                     //34
+                     //35
+                     //36
+                     //37
+                     //38
+                     //39
+                     //40
+                     //41
+                     //42
+
+         + "    FROM       pagos p, documentos d,  tiposdocumento td, cajas c,  clientes cl, creditos cr,  tipo_documento td2,"
+   + " tipo_cambio tc WHERE p.pagoid = d.pagoid(+)  AND d.tipodocumentoid = td.tipodocumentoid(+)  AND td.tipo_doc = td2.id_tipodoc(+)"
+     + "   AND td.cajaid = c.cajaid(+)  AND p.creditoid = cr.creditoid(+)  AND cr.clienteid = cl.clienteid(+) and trunc(d.fechareal) = trunc(tc.fechaingreso)  "
+     + "    AND c.seriedocumentos is not null  "
+     + " and  d.numero='2247370'  "
+     + "    order by d.documentoid asc ";
+
+            OracleCommand Cmd = new OracleCommand();
+            OracleDataAdapter Dap = new OracleDataAdapter();
+            Cmd.CommandText = sSQL;
+            Cmd.CommandType = CommandType.Text;
+            Cmd.Connection = Cn;
+
+            ///llenar datagridview
+            //Dap.SelectCommand = Cmd;
+            //DataTable Dt = new DataTable();
+            //Dap.Fill(Dt);
+            //return Dt;
+
+            /// 
+
+
+
+
+            OracleDataReader Dr;
+
+            Dr = Cmd.ExecuteReader();
+
+            if (Dr.HasRows)
+            {
+                Dr.Read();
+
+                doc.FechaEmision = Dr["IssueDate"].ToString();
+                doc.Emisor.NombreLegal = Dr["RegistrationName"].ToString();
+                doc.TotalVenta = Convert.ToDecimal(Dr["PayableAmount"].ToString());
+                doc.TotalOtrosTributos = 0;
+                doc.TotalIsc = 0;
+                doc.TotalIgv = 32;
+                doc.TipoOperacion = "";
+                doc.TipoDocumento = Dr["InvoiceTypeCode"].ToString();
+
+                doc.Receptor.NombreComercial = "";
+                doc.Receptor.NombreLegal = "armestar merino chirtofer";
+                doc.Receptor.NroDocumento = "42544373";
+                doc.Receptor.Provincia = "01";
+                doc.Receptor.TipoDocumento = "01";
+                doc.Receptor.Ubigeo = "200101";
+                doc.Receptor.Urbanizacion = "mananzales";
+                doc.Receptor.Distrito = "20";
+                doc.Receptor.Direccion = "sucasa ";
+                doc.Receptor.Departamento = "20";
+
+                doc.MontoEnLetras = "cuanrenta y cinco";
+                doc.Moneda = "PEN";
+                doc.Inafectas = 2;
+                doc.IdDocumento = Dr["ID"].ToString();
+                doc.Gravadas = 455;
+                doc.Gratuitas = 2;
+                doc.Exoneradas = 12;
+                doc.CalculoIsc = 125;
+                doc.CalculoIgv = 22;
+                doc.CalculoDetraccion = 21;
+                doc.Emisor.NombreComercial = "INVERSIONES ALBUSA";
+
+                doc.Emisor.NroDocumento = "20484205249";
+                doc.Emisor.Provincia = "01";
+                doc.Emisor.TipoDocumento = "ew";
+                doc.Emisor.Ubigeo = "200101";
+                doc.Emisor.Urbanizacion = "Jr Junin 817";
+                doc.Emisor.Distrito = "20";
+                doc.Emisor.Direccion = "Jr junin";
+                doc.Emisor.Departamento = "20";
+                //  doc.Items.Add()
+
+
+
+                return true;
+            }
+            else
+            {
+                Dr.Close();
+
+                return false;
+            }
+        }
+
+        ///
 
         public object Clone()
         {
--- iAlbusaForm/Form1.cs
+++ iAlbusaForm/Form1.cs
@@ -67,7 +67,7 @@ namespace  iAlbusaForm
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+           
 
             try
             {
